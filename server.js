0ce0752cd4d9cd1d3274957f95fcb28007aca1ab
gagikm@ucla.edu <gagikm@ucla.edu>
2018-01-30 16:16:07 -0800

    Remove heroku ssl redirect

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var favicon = require('serve-favicon');
 var http = require('http');
 var logger = require('morgan');
 var path = require('path');
-var sslRedirect = require('heroku-ssl-redirect');
 
 var app = express();
 
@@ -17,8 +16,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
-
-// app.use(sslRedirect());
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
