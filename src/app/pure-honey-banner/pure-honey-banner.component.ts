1e6a595c6ea5e6731f11a7d5e14933db012e6643
Gagik Movsisyan <gagikm@ucla.edu>
2021-01-03 18:20:41 -0800

    Add pure honey banner

---
+++ src/app/pure-honey-banner/pure-honey-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pure-honey-banner',
+  templateUrl: './pure-honey-banner.component.html',
+  styleUrls: ['./pure-honey-banner.component.css']
+})
+export class PureHoneyBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
